6c6db058b731427ecbfa388aeac13189fd7e65dc
Josh Lee <joshua.huggins@sbcglobal.net>
2024-05-06 22:46:52 -0500

    all backend issues are fixed

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -9,8 +9,8 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-//   await seedUsers();
-//   console.log('\n----- USERS SEEDED -----\n');
+  await seedUsers();
+  console.log('\n----- USERS SEEDED -----\n');
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
--- src/seeds/user-seeds.js
+++ src/seeds/user-seeds.js
@@ -1,31 +1,33 @@
 const { User } = require('../models');
 
-const userData = require('./userData.json');
-
-[
+const userData = [
     {
-      "username": "John Smith",
-      "email": "jsmith@hotmail.com",
-      "password": "password12345"
+        username: "John Smith",
+        email: "jsmith@hotmail.com",
+        password: "password12345"
     },
     {
-      "username": "Sus Luis",
-      "email": "susluis@gmail.com",
-      "password": "password12345"
+        username: "Sus Luis",
+        email: "susluis@gmail.com",
+        password: "password12345"
     },
     {
-      "username": "Blake Hudson",
-      "email": "blake@aol.com",
-      "password": "password12345"
+        username: "Blake Hudson",
+        email: "blake@aol.com",
+        password: "password12345"
     },
     {
-      "username": "James Wilson",
-      "email": "jwill99@msn.com",
-      "password": "password12345"
+        username: "James Wilson",
+        email: "jwill99@msn.com",
+        password: "password12345"
     },
     {
-      "username": "Laura Croft",
-      "email": "tombraider@yahoo.com",
-      "password": "password12345"
+        username: "Laura Croft",
+        email: "tombraider@yahoo.com",
+        password: "password12345"
     }
-  ]
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
